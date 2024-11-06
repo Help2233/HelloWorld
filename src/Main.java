65d031d280f675bd0fd8d66bdceb774db768612c
help1 <help13341@gmail.com>
2024-11-06 17:36:11 +0400

    fixed the error

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ import static java.lang.System.setOut;
 public class Main {
     public static void main(String[] args) {
         var dog = 8.0;
+        out.println(dog);
         var cat = 3.6;
+        out.println(cat);
         var paper = 763789;
+        out.println(paper);
         dog = dog + 4;
         out.println(dog);
         cat = cat + 4;
